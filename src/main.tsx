39dc514f52e8145e7aa6542cddcc0a558ce37dcf
rahathossenantor <md.rahathossenantor@gmail.com>
2024-08-21 01:08:39 +0600

    Redux persist implemented

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import baseApi from "./api/baseApi";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "auth",
+    storage
+};
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
-        auth: authReducer,
+        auth: persistedAuthReducer,
     },
-    middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(baseApi.middleware)
+    middleware: (getDefaultMiddlewares) => getDefaultMiddlewares({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }).concat(baseApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
+
 export default store;

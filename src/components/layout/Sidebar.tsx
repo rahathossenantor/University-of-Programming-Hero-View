374a70e9e21f8c7ccc473adeb2f3bfd0f51bbcd3
rahathossenantor <md.rahathossenantor@gmail.com>
2024-08-20 22:18:51 +0600

    Implemented sidebar items based on role

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,9 +1,37 @@
 import { Layout, Menu } from "antd";
+import { TRole } from "../../types/types.global";
 import { adminSidebarItems } from "../../routes/admin.routes";
+import { facultySidebarItems } from "../../routes/faculty.routes";
+import { studentSidebarItems } from "../../routes/student.routes";
 
 const { Sider } = Layout;
 
+const userRoles = {
+    SUPER_ADMIN: "super-admin",
+    ADMIN: "admin",
+    FACULTY: "faculty",
+    STUDENT: "student",
+};
+
 const Sidebar = () => {
+    // implement sidebar items based on role
+    const role: TRole = "admin";
+    let sidebarItems;
+    switch (role) {
+        case userRoles.ADMIN:
+            sidebarItems = adminSidebarItems;
+            break;
+        case userRoles.FACULTY:
+            sidebarItems = facultySidebarItems;
+            break;
+        case userRoles.STUDENT:
+            sidebarItems = studentSidebarItems;
+            break;
+    
+        default:
+            break;
+    };
+
     return (
         <Sider
             breakpoint="lg"
@@ -27,7 +55,7 @@ const Sidebar = () => {
             >
                 <h1>University</h1>
             </div>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]} items={adminSidebarItems} />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]} items={sidebarItems} />
         </Sider>
     );
 };
